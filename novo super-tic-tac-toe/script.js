02c1084eeb3e57dab33407b9baa44796a90ec9ed
Godofredo Dantas de Medeiros Maia <20231101110038@ifrn.local>
2024-02-29 14:06:10 -0300

    ficou melhorzin

--- novo super-tic-tac-toe/script.js
+++ novo super-tic-tac-toe/script.js
@@ -17,3 +17,8 @@ document.body.appendChild(big_board)
 let small_board = create_elements_from_html(9, `<div class="small_board" data-cell></div>`)
 
 document.body.querySelector(".big_board").appendChild(small_board)
+
+let cell = create_elements_from_html(9, `
+        <div class="cell o" data-cell"></div>`)
+document.querySelectorAll(".small_board").forEach((board) => board.appendChild(cell.cloneNode(true)))
+//Criando todos as células, jogos da velha pequenos e o jogo da velha grande
