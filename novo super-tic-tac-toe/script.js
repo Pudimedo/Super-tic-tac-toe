04bd6fc7f0b4bc827c5f02bfa408594e5af7c344
Godofredo Dantas de Medeiros Maia <20231101110038@ifrn.local>
2024-03-07 11:55:53 -0300

    mark place e hover

--- novo super-tic-tac-toe/script.js
+++ novo super-tic-tac-toe/script.js
@@ -14,15 +14,46 @@ let big_board = create_elements_from_html(1, `<div class="big_board"></div>`)
 
 document.body.appendChild(big_board)
 
-let small_board = create_elements_from_html(9, `<div class="small_board x o" data-cell></div>`)
+let small_board = create_elements_from_html(9, `<div class="small_board"></div>`)
 
 document.body.querySelector(".big_board").appendChild(small_board)
 
 let cell = create_elements_from_html(9, `
-        <div class="cell" data-cell"></div>`)
+        <div class="cell" data-cell ></div>`)
 document.querySelectorAll(".small_board").forEach((board) => board.appendChild(cell.cloneNode(true)))
 //Criando todos as células, jogos da velha pequenos e o jogo da velha grande
 
-//document.querySelector(".cell").forEach((board) => board.addEventListener("mouseover", (event){
-    
-//}))
+const x_class = "x"
+const o_class = "o"
+const cell_elements = document.querySelectorAll("[data-cell]")
+let o_turn
+const board = document.querySelectorAll(".small_board")
+
+cell_elements.forEach(cell => {
+    cell.addEventListener("click", handle_click, {once: true})
+})
+
+function handle_click(e){
+    const cell = e.target
+    const current_class = o_turn ? o_class : x_class
+    place_mark(cell, current_class)
+    swap_turns()
+    set_board_hover_class()
+}
+
+function place_mark(cell, current_class){
+    cell.classList.add(current_class)
+}
+
+function swap_turns(){
+    o_turn = !o_turn
+}
+
+function set_board_hover_class(){
+    if (o_turn){
+        board.classList.add(o_class)
+    }
+    else{
+        board.classList.add(x_class)
+    }
+}
